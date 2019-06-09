2323e3000e142e89331371af8fef559d1becc871
武斌斌 <50254643+shuangyuwbb@users.noreply.github.com>
2019-06-09 12:26:56 +0800

    Add files via upload

---
+++ myproject002/demo.js
@@ -0,0 +1,69 @@
+var div1=document.getElementsByClassName("bc1")[0];
+var img=document.createElement("img");
+div1.append(img);
+var div=document.getElementsByClassName("bc")[0];
+var div2=div.getElementsByTagName("div");
+div2[0].onmousedown=function(e)
+{
+    var Img=div2[0].getElementsByTagName("img")[0];
+    var src=Img.getAttribute("src");
+    img.src=Img.src;
+    img.style.width="832.6px";
+}
+div2[1].onmousedown=function(e)
+{
+    var Img=div2[1].getElementsByTagName("img")[0];
+    var src=Img.getAttribute("src");
+    img.src=Img.src;
+    img.style.width="832.6px";
+}
+div2[2].onmousedown=function(e)
+{
+    var Img=div2[2].getElementsByTagName("img")[0];
+    var src=Img.getAttribute("src");
+    img.src=Img.src;
+    img.style.width="832.6px";
+}
+div2[3].onmousedown=function(e)
+{
+    var Img=div2[3].getElementsByTagName("img")[0];
+    var src=Img.getAttribute("src");
+    img.src=Img.src;
+    img.style.width="832.6px";
+}
+div2[4].onmousedown=function(e)
+{
+    var Img=div2[4].getElementsByTagName("img")[0];
+    var src=Img.getAttribute("src");
+    img.src=Img.src;
+    img.style.width="832.6px";
+}
+div2[5].onmousedown=function(e)
+{
+    var Img=div2[5].getElementsByTagName("img")[0];
+    var src=Img.getAttribute("src");
+    img.src=Img.src;
+    img.style.width="832.6px";
+}
+div2[6].onmousedown=function(e)
+{
+    var Img=div2[6].getElementsByTagName("img")[0];
+    var src=Img.getAttribute("src");
+    img.src=Img.src;
+    img.style.width="832.6px";
+}
+div2[7].onmousedown=function(e)
+{
+    var Img=div2[7].getElementsByTagName("img")[0];
+    var src=Img.getAttribute("src");
+    img.src=Img.src;
+    img.style.width="832.6px";
+}
+div2[8].onmousedown=function(e)
+{
+    var Img=div2[8].getElementsByTagName("img")[0];
+    var src=Img.getAttribute("src");
+    img.src=Img.src;
+    img.style.width="832.6px";
+}
+
